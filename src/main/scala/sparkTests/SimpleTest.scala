a79a96c68208204c3d1c29d59f20d67413850263
Vinay Mishra <vinay@roambee.com>
2016-05-18 13:49:26 -0700

    test

--- src/main/scala/sparkTests/SimpleTest.scala
+++ src/main/scala/sparkTests/SimpleTest.scala
@@ -7,20 +7,20 @@ object SimpleTest extends App{
 
 
   //simpleTest
-  simpleTestSubmit
+  //simpleTest1()
+  simpleTest1Submit
 
-  def simpleTest() {
-    /*val conf = new SparkConf().
-    setMaster("spark://stationrotation.corp.ne1.yahoo.com:7077").setAppName("My app")
-    */
-    val conf = new SparkConf().setMaster("local").setAppName("My within eclipse app")
+
+  def simpleTest1Submit() {
+
+    val conf = new SparkConf().setAppName("My app")
     val sc = new SparkContext(conf)
-    val data = 1 to 1000
-    val distData = sc.parallelize(data)
-    println("Storage level of this RDD distData is " + distData.getStorageLevel)
-    distData.filter(_ < 10).collect().foreach(println)
+    val distData = sc.parallelize(1 to 10)
+    distData.collect().foreach(println)
     println(distData.toDebugString)
     sc.stop()
+
+
   }
 
 
@@ -37,6 +37,36 @@ object SimpleTest extends App{
     distData.filter(_ < 10).collect().foreach(println)
     println(distData.toDebugString)
     Thread.sleep(10000)
+    sc.stop()
+
+  }
+
+
+
+  def simpleTest() {
+    /*val conf = new SparkConf().
+    setMaster("spark://stationrotation.corp.ne1.yahoo.com:7077").setAppName("My app")
+    */
+    val conf = new SparkConf().setMaster("local").setAppName("My within eclipse app")
+    val sc = new SparkContext(conf)
+    val data = 1 to 1000
+    val distData = sc.parallelize(data)
+    println("Storage level of this RDD distData is " + distData.getStorageLevel)
+    distData.filter(_ < 10).collect().foreach(println)
+    println(distData.toDebugString)
+    sc.stop()
+  }
+
+  def simpleTest1() {
+    /*val conf = new SparkConf().
+    setMaster("spark://stationrotation.corp.ne1.yahoo.com:7077").setAppName("My app")
+    */
+
+    val conf = new SparkConf().setMaster("local").setAppName("My within eclipse app")
+    val sc = new SparkContext(conf)
+    val distData = sc.parallelize(1 to 10)
+    distData.collect().foreach(println)
+    println(distData.toDebugString)
     sc.stop()
 
   }
